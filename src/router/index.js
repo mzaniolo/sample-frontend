4a84599f0179d3081a968a87e8e78744f1e2edfc
Matheus Zaniolo <mgzaniolo@gmail.com>
2025-07-11 19:19:31 +0200

    Add pr preview

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import DashboardView from '../views/DashboardView.vue'
 import CallbackView from '../views/CallbackView.vue'
@@ -24,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes
 })
 
